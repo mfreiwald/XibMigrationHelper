e5824a4e27e3b16ffb7f271b8f9bdccd03c70296
Michael Freiwald <michael.freiwald@zooplus.com>
2024-07-15 13:50:03 +0200

    Update IBDecodable dependency to fork

--- Package.swift
+++ Package.swift
@@ -7,20 +7,16 @@ let package = Package(
     name: "XibMigrationHelper",
     platforms: [.macOS(.v14)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .executable(name: "XibMigrationHelper", targets: ["XibMigrationHelper"])
     ],
     dependencies: [
-        .package(path: "IBDecodable"),
-//        .package(url: "https://github.com/IBDecodable/IBDecodable", branch: "master"),
+        .package(url: "https://github.com/mfreiwald/IBDecodable", branch: "xib-migration"),
         .package(url: "https://github.com/JohnSundell/Files", branch: "master"),
         .package(url: "https://github.com/scottrhoyt/SwiftyTextTable", branch: "master"),
         .package(url: "https://github.com/onevcat/Rainbow", branch: "master"),
         .package(url: "https://github.com/apple/swift-argument-parser", branch: "main")
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
             name: "XibMigrationHelper",
             dependencies: [
